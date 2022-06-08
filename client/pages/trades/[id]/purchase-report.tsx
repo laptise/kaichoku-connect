55bc50346bf089f1a18410f998f3ce6f546211b9
Yoonsoo Kim <laptise@live.jp>
2022-06-08 22:03:57 +0900

    商品購買完了登録ページ

--- client/pages/trades/[id]/purchase-report.tsx
+++ client/pages/trades/[id]/purchase-report.tsx
@@ -1,5 +1,29 @@
-const PurchaseReport = () => {
-  return <div>da</div>;
+import { Trade } from "@entities";
+import { GetServerSideProps } from "next";
+import client from "../../../apollo-client";
+import Layout from "../../../components/layout";
+import { requireAuth } from "../../../components/use-auth";
+import { AuthNextPage, AuthRequiredPage } from "../../../env";
+import { GET_TRADE_BY_ID } from "../../../gqls/queries/trade";
+
+type SingleTradeProps = {
+  trade: Trade;
+};
+
+const PurchaseReport: AuthRequiredPage<SingleTradeProps> = ({ trade, payload }) => {
+  return (
+    <Layout pageTitle={"商品手配完了のお知らせ"} mainId={""}>
+      <div>{trade.tradeRequest?.title}</div>
+    </Layout>
+  );
 };
 
 export default PurchaseReport;
+
+export const getServerSideProps: GetServerSideProps = (ctx) =>
+  requireAuth(ctx, async ({ params, payload }) => {
+    const res = await client
+      .query<NestedQuery<"getTradeById", Trade>>({ query: GET_TRADE_BY_ID, variables: { id: Number(params!.id) } })
+      .then((x) => x.data.getTradeById);
+    return { props: { trade: res, payload } };
+  });
